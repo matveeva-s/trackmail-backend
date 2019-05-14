078eb4cae65a909be198497a9e9d04125687ed3b
matveeva-s <matveeva.svetlana@gmail.com>
2019-05-14 12:58:06 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'matveeva.svetlana@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ONEandone12'
+app.config['MAIL_PASSWORD'] = '***********'
 app.config['ADMINS'] = ['matveeva.svetlana@gmail.com']
 
 
